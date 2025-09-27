d6910ea8c1950fbcd7477d16a17304d543b9db71
Gertvaaborta <lololo822228822228822228@gmail.com>
2025-09-27 20:19:14 +0300

    tochnolaba.cpp

--- main (3).cpp
+++ main (3).cpp
@@ -1,21 +1,8 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include "otvetb.h"
 using namespace std;
-string otvet(string a){
-    string otv = "";
-    string glasnie = "aeiouAEIOU";
-    int i = 0;
-    for (int i=0; i<a.length(); i++) {
-        bool glas=false;
-        for (int j=0; j<glasnie.length();j++) {
-            if (a[i]==glasnie[j]) {
-                glas = true;break;
-            }
-        }
-        if (!glas) otv+=a[i];
-    }
-    return otv;
-}
+
 int main()
 {
-    string a;cin>>a;cout<<otvet(a)<<"\n";
+    string a; cin >> a; cout << otvet(a) << "\n";
 }
