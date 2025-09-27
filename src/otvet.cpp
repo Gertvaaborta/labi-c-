c5727c0590cfbee1fde7933c800f7834ee614b0a
Gertvaaborta <lololo822228822228822228@gmail.com>
2025-09-27 20:19:50 +0300

    otvet.cpp

--- src/otvet.cpp
+++ src/otvet.cpp
@@ -1,17 +1,18 @@
-#include "otvet.h"
+#include "otvetb.h"
 
-std::string otvet(std::string a){
+std::string otvet(std::string a) {
     std::string otv = "";
     std::string glasnie = "aeiouAEIOU";
-    int i = 0;
-    for (int i=0; i<a.length(); i++) {
-        bool glas=false;
-        for (int j=0; j<glasnie.length();j++) {
-            if (a[i]==glasnie[j]) {
-                glas = true;break;
+
+    for (size_t i = 0; i < a.length(); i++) {
+        bool glas = false;
+        for (size_t j = 0; j < glasnie.length(); j++) {
+            if (a[i] == glasnie[j]) {
+                glas = true;
+                break;
             }
         }
-        if (!glas) otv+=a[i];
+        if (!glas) otv += a[i];
     }
     return otv;
 }
